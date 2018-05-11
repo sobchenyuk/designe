cb3996a765e41f18e792d0974e389388b974d41b
Natali <ver4.nata@gmail.com>
2018-05-11 17:26:09 +0300

    theird commit

--- js/script.js
+++ js/script.js
@@ -1,13 +1,55 @@
 
 $(document).ready(function() {
-    var header = $('.header-wrapper');
-    var h = header.height();
+    //scroll
+    (function(){
+
+        var header = $('.header-wrapper');
+        var h = header.height();
+        
+        $(window).scroll(function(){
+        if($(this).scrollTop()>h){
+                header.addClass('sticky')
+            } else if ($(this).scrollTop()<h){
+                header.removeClass('sticky');
+            }
+        }); 
+
+    })($);
+
+    (function(){
+
+        var form = document.querySelector('#form');
+
+        form.onsubmit = function(e){
+
+            var validate = this.querySelectorAll('.validate');
+            var a = 0;
+
+            validate.forEach(function(item, i) {
     
-    $(window).scroll(function(){
-       if($(this).scrollTop()>h){
-            header.addClass('sticky')
-        } else if ($(this).scrollTop()<h){
-            header.removeClass('sticky');
+                if(item.value === ""){
+    
+                    item.parentElement.classList.add('invalid');
+                    a++;
+
+                } else {
+
+                    if(item.parentElement.classList.contains('invalid')){
+                        item.parentElement.classList.remove('invalid');
+                    }
+
+                }
+
+    
+            });
+
+            if(a > 0){
+                e.preventDefault();
+            }
+
         }
-    });
+
+    })()
+
+    
 });
